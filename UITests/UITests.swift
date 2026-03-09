3db1b14434a5e2fc963b0f7a4b27056b286963ea
sheyn1976 <ilya.sheynblat@testpro.io>
2026-03-09 15:55:53 -0400

    add test and HWtest

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -3,20 +3,65 @@
 //  UITests
 //
 //  Created by Ilya Sheynblat on 2/19/26.
-//
+//NavigationTabBar Testing!:
 
 import XCTest
 
-final class UITests: XCTestCase {
-    let app = XCUIApplication()
+class UITests: Runner {
+    // MARK: Static Texts
+    lazy var homeScreenTitle: XCUIElement = app.staticTexts["homeView_title"]
+    lazy var bibleStudyScreenTitle: XCUIElement = app.staticTexts[" Growth in Faith"]
     
-    override func setUpWithError() throws { //b4 every test
-        continueAfterFailure = false
-        app.launch()
+    // MARK: TextFields
+    lazy var feedScreenSearchField: XCUIElement = app.textFields["Search users or posts"]
+    
+    // MARK: Buttons
+    lazy var exploreScreenQuizCell: XCUIElement = app.buttons["📖 Faith-Based Quiz"]
+    
+    //MARK: TabBar
+    lazy var homeTab: XCUIElement = app.tabBars.buttons["Home"]
+    lazy var bibleStudyTab: XCUIElement = app.tabBars.buttons["Bible Study"]
+    lazy var feedTab: XCUIElement = app.tabBars.buttons["Feed"]
+    lazy var exploreTab: XCUIElement = app.tabBars.buttons["Explore"]
+    
+    // MARK: Tests
+    func testMainScreens() {
+        homeScreenTitle.assertExistence()
+        homeTab.assertExistence()
+        
+        bibleStudyTab.tap()
+        bibleStudyTab.assertExistence()
+        bibleStudyScreenTitle.assertExistence()
+        
+        feedTab.tap()
+        feedTab.assertExistence()
+        feedScreenSearchField.assertExistence()
+        
+        exploreTab.tap()
+        exploreTab.assertExistence()
+        exploreScreenQuizCell.assertExistence()
     }
     
-    override func tearDownWithError() throws {}  // after every test
-    
-    func testExample() throws {
+    func testNavigationTabBar() {
+        // homeTab isSelected
+        // app.tabBars.count == 4
+        
+        // bibleStudyTab.tap()
+        // app.tabBars.count == 4
+        //bibleStudyTab button is Selected
+        
+        // feedTab.tap()
+        // app.tabBars.count == 4
+        //feed button is Selected
+        
+        // explore.tap()
+        // app.tabBars.count == 4
+        //bibleStudyTab button is Selected
+        
+        //homeTab.tap()
+        //app.tabBars.count == 4
+        // homeTab isSelected
+        
     }
 }
+
