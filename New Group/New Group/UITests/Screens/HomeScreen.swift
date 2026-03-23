6b55d78da54eb8f68743a1ef103b1a76a56dc866
sheyn1976 <ilya.sheynblat@testpro.io>
2026-03-22 20:09:35 -0400

    [UI Test] Adds testNavigationHomeScreen()
    testNavigationHomeScreen() b4 refactoring

--- New Group/New Group/UITests/Screens/HomeScreen.swift
+++ New Group/New Group/UITests/Screens/HomeScreen.swift
@@ -7,88 +7,15 @@
 import XCTest
 
 final class HomeScreen: BaseScreen {
-    
-   // MARK: Buttons
-    lazy var bibleReading: XCUIElement = app.buttons["Bible Reading"]
-    lazy var backButton: XCUIElement = app.buttons["Back"]
-    lazy var profile: XCUIElement = app.buttons["Profile"]
-    lazy var donation: XCUIElement = app.buttons["Donation"]
-    lazy var multiSelection: XCUIElement = app.buttons["Multi-selection"]
-    lazy var holyPlacesMap: XCUIElement = app.buttons["Holy Places Map"]
-    lazy var plusCircle: XCUIElement = app.buttons["plus.circle.fill"]
-    lazy var liveStream: XCUIElement = app.buttons["Live Stream"]
-    
-    // MARK: Images
-    lazy var bibleReadingIcon: XCUIElement = app.images["book"]
-    lazy var profileIcon: XCUIElement = app.images["person.circle"]
-    lazy var donationIcon: XCUIElement = app.images["creditcard"]
-    lazy var multiSelectionIcon: XCUIElement = app.images["square.stack.3d.down.forward"]
-    lazy var holyPlacesMapIcon: XCUIElement = app.images["map"]
-    lazy var liveStreamIcon: XCUIElement = app.images["play.circle"]
-    
-    // MARK: TextFields
-    lazy var bibleReadingSearchField: XCUIElement = app.textFields.firstMatch
-    
-    // MARK: Static Texts
-    //lazy var homeScreenTitle: XCUIElement = app.staticTexts["homeView_title"]
-    lazy var donationTitle: XCUIElement = app.staticTexts
-        .matching(NSPredicate(format: "label BEGINSWITH 'Total Amount'"))
-        .element
-    lazy var multiSelectionTitle: XCUIElement = app.staticTexts["Select Activities"]
-    
-    // MARK: NavigationBars
-    lazy var navigationBarTitle: XCUIElement = app.navigationBars["Profile"]
-    
-    
-    func testNavigationHomeScreen() {
-        bibleReadingIcon.assertExistence()
-        bibleReading.tap()
-        
-        bibleReadingSearchField.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-        
-        profileIcon.assertExistence()
-        profile.tap()
-        navigationBarTitle.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-        
-        donationIcon.assertExistence()
-        donation.tap()
-        donationTitle.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-        
-        liveStreamIcon.assertExistence()
-        liveStream.tap()
-        backButton.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-        
-        multiSelectionIcon.assertExistence()
-        multiSelection.tap()
-        multiSelectionTitle.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-        
-        holyPlacesMapIcon.assertExistence()
-        holyPlacesMap.tap()
-        plusCircle.assertExistence()
-        backButton.tap()
-        homeScreenTitle.assertExistence()
-    }
-   
     lazy var homeScreenTitle: XCUIElement = app.staticTexts["homeView_title"]
-    lazy var bibleStudyScreenTitle: XCUIElement = app.staticTexts[" Growth in Faith"]
     
     // MARK: Then
-     func thenIAssertScreen() {
-         homeScreenTitle.assertExistence()
-         
+    func thenIAssertScreen() {
+        homeScreenTitle.assertExistence()
+        
     }
     // MARK: When
     
-    }
-    
+}
+
 
--- New Group/New Group/UITests/UITests.swift
+++ New Group/New Group/UITests/UITests.swift
@@ -39,6 +39,7 @@ class UITests: Runner {
         homeScreen.thenIAssertScreen()
     }
     
+    
     func testNavigationTabBar() {
         navigationTabBar.thenHomeTabAppears()
         navigationTabBar.thenHomeTabSelected()
@@ -57,16 +58,88 @@ class UITests: Runner {
         navigationTabBar.thenAllTabsAppears()
         
         navigationTabBar.whenINavigateToHomeTab()
-       // XCTAssertTrue(navigationTabBar.homeTab.isSelected)
         navigationTabBar.thenHomeTabSelected()
-       // XCTAssertTrue(app.tabBars.buttons.count == 4)
-       // app.tabBars.buttons.assertEqual(4)
         navigationTabBar.thenAllTabsAppears()
     }
+    
+    // b4
+    // assert(element, error massage)
+    //  XCTAssertEqual(app.tabBars.buttons.count, 4)
+    
+    // After
+    // given, then,when contains -> element.assert()
+    
+    
+    
+    // MARK: Buttons
+    lazy var bibleReading: XCUIElement = app.buttons["Bible Reading"]
+    lazy var backButton: XCUIElement = app.buttons["Back"]
+    lazy var profile: XCUIElement = app.buttons["Profile"]
+    lazy var donation: XCUIElement = app.buttons["Donation"]
+    lazy var multiSelection: XCUIElement = app.buttons["Multi-selection"]
+    lazy var holyPlacesMap: XCUIElement = app.buttons["Holy Places Map"]
+    lazy var plusCircle: XCUIElement = app.buttons["plus.circle.fill"]
+    lazy var liveStream: XCUIElement = app.buttons["Live Stream"]
+    
+    // MARK: Images
+    lazy var bibleReadingIcon: XCUIElement = app.images["book"]
+    lazy var profileIcon: XCUIElement = app.images["person.circle"]
+    lazy var donationIcon: XCUIElement = app.images["creditcard"]
+    lazy var multiSelectionIcon: XCUIElement = app.images["square.stack.3d.down.forward"]
+    lazy var holyPlacesMapIcon: XCUIElement = app.images["map"]
+    lazy var liveStreamIcon: XCUIElement = app.images["play.circle"]
+    
+    // MARK: TextFields
+    lazy var bibleReadingSearchField: XCUIElement = app.textFields.firstMatch
+    
+    // MARK: Static Texts
+    lazy var homeScreenTitle: XCUIElement = app.staticTexts["homeView_title"]
+    lazy var donationTitle: XCUIElement = app.staticTexts
+        .matching(NSPredicate(format: "label BEGINSWITH 'Total Amount'"))
+        .element
+    lazy var multiSelectionTitle: XCUIElement = app.staticTexts["Select Activities"]
+    
+    // MARK: NavigationBars
+    lazy var navigationBarTitle: XCUIElement = app.navigationBars["Profile"]
+    
+    
+    func testNavigationHomeScreen() {
+       // homeScreenTitle.assertExistence()
+        homeScreen.thenIAssertScreen()
+        bibleReadingIcon.assertExistence()
+        bibleReading.tap()
+        bibleReadingSearchField.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+        
+        profileIcon.assertExistence()
+        profile.tap()
+        navigationBarTitle.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+        
+        donationIcon.assertExistence()
+        donation.tap()
+        donationTitle.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+        
+        liveStreamIcon.assertExistence()
+        liveStream.tap()
+        backButton.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+        
+        multiSelectionIcon.assertExistence()
+        multiSelection.tap()
+        multiSelectionTitle.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+        
+        holyPlacesMapIcon.assertExistence()
+        holyPlacesMap.tap()
+        plusCircle.assertExistence()
+        backButton.tap()
+        homeScreenTitle.assertExistence()
+    }
 }
-// b4
-// assert(element, error massage)
-//  XCTAssertEqual(app.tabBars.buttons.count, 4)
-
-// After
-// given, then,when contains -> element.assert()
